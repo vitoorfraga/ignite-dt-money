bd24037bd35788d272650eb9efeaa53c0c3f8e89
Vitor Fraga <vitorfragaps@gmail.com>
2023-08-19 14:50:07 -0300

    Estrutura visual - Tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,41 @@
 import Header from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHightlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export default function Transactions() {
   return (
     <>
       <Header/>
       <Summary />
+
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHightlight variant="income">
+                  R$ 12.000,00
+                </PriceHightlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHightlight variant="outcome">
+                  - R$ 59,00
+                </PriceHightlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </>
   )
 }
